7c9b22b3fadbe9e40c0f26d7d5698e7ed83c3fa3
Hamodzpej <51094114+Hamodzpej@users.noreply.github.com>
2019-05-27 11:11:45 -0700

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,130 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const myid = [''];
+const prefix = ['-'];
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+client2.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+
+
+
+client.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + 503547627346984960 + ">")
+    }
+});
+
+client.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By 3mo Hamad , Farm Credit By ElKaber Hamaaad, ez Farm - فــــــــــــــــــــآنز حـــمـــد **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
+client2.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send('#rep <@286088294234718209>')
+    }
+});
+
+client2.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Hamad , Love Hamd :), ez Farm - Faaaanz Hamaddddddddddd**[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
+
+client.login('NTgxMjE4NTc2NTY1MDEwNDQz.XOcFPw.FE-arutDwldMHgGHezNTgomqFzw');
